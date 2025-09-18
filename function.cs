a912eafd6138c07dacfcedf4128535599ec53c33
Melkzedek <79189172+Melkzedk@users.noreply.github.com>
2025-09-18 22:48:28 +0300

    more changes

--- function.cs
+++ function.cs
@@ -1,7 +1,26 @@
-
+/*
 static void MyMethod ()
 {
     Console.WriteLine("This is my method.");
 }
 
 MyMethod();  // Call the method
+
+*/
+
+static int PlusMethod(int x, int y)
+{
+  return x + y;
+}
+
+static double PlusMethod(double x, double y)
+{
+    return x + y;
+}
+
+int myNum1 = PlusMethod(8, 5);
+  double myNum2 = PlusMethod(4.3, 6.26);
+  Console.WriteLine("Int: " + myNum1);
+  Console.WriteLine("Double: " + myNum2);
+
+  
