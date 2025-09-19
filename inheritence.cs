443f9392c4cd968e40f4743ab965adef94f78d04
Melkzedek <79189172+Melkzedk@users.noreply.github.com>
2025-09-19 07:47:39 +0300

    changes

--- inheritence.cs
+++ inheritence.cs
@@ -1,28 +1,22 @@
 class Vehicle  // base class (parent) 
 {
-  public string brand = "Ford";  // Vehicle field
-  public void honk()             // Vehicle method 
-  {                    
-    Console.WriteLine("Tuut, tuut!");
-  }
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {                    
+        Console.WriteLine("Tuut, tuut!");
+    }
 }
 
 class Car : Vehicle  // derived class (child)
 {
-  public string modelName = "Mustang";  // Car field
+    public string modelName = "Mustang";  // Car field
 }
 
-class Program
-{
-  static void Main(string[] args)
-  {
-    // Create a myCar object
-    Car myCar = new Car();
+// --- Top-level code ---
+Car myCar = new Car();
 
-    // Call the honk() method (From the Vehicle class) on the myCar object
-    myCar.honk();
+myCar.honk();
 
-    // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
-    Console.WriteLine(myCar.brand + " " + myCar.modelName);
-  }
-}
+Console.WriteLine(myCar.brand + " " + myCar.modelName);
+
+// Outputs: Ford Mustang
