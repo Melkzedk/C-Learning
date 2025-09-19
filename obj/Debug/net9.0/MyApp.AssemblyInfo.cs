8bc5b74dde2c1e323464111a0a5347f92dfa85ee
Melkzedek <79189172+Melkzedk@users.noreply.github.com>
2025-09-19 08:08:35 +0300

    variable

--- obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a912eafd6138c07dacfcedf4128535599ec53c33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443f9392c4cd968e40f4743ab965adef94f78d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ variables.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Variable
+{
+	static void Main()
+	{
+		string name = "Alice";
+		int age = 30;
+
+		Console.WriteLine($"Name: {name}, Age: {age}");
+	}
+}
