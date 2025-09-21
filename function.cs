a068a42ae22d1e4bb175fc4990676318407ed76d
Melkzedk <melkywafula29@gmail.com>
2025-09-21 19:18:58 +0300

    more changes

--- function.cs
+++ function.cs
@@ -11,6 +11,7 @@ MyMethod();  // Call the method
 static int PlusMethod(int x, int y)
 {
   return x + y;
+  return a + b;
 }
 
 static double PlusMethod(double x, double y)
@@ -22,5 +23,7 @@ int myNum1 = PlusMethod(8, 5);
   double myNum2 = PlusMethod(4.3, 6.26);
   Console.WriteLine("Int: " + myNum1);
   Console.WriteLine("Double: " + myNum2);
+
+  //
 
   
--- variables.cs
+++ variables.cs
@@ -6,7 +6,9 @@ class Variable
 	{
 		string name = "Alice";
 		int age = 30;
+		string lastname = "Doe";
 
 		Console.WriteLine($"Name: {name}, Age: {age}");
+		Console.WriteLine(lastname);
 	}
 }
