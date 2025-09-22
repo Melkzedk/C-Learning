fb00d92df828611c98dc32ff5838f94f822987ce
Melkzedk <melkywafula29@gmail.com>
2025-09-22 21:13:16 +0300

    more changes

--- function.cs
+++ function.cs
@@ -23,9 +23,3 @@ int myNum1 = PlusMethod(8, 5);
   double myNum2 = PlusMethod(4.3, 6.26);
   Console.WriteLine("Int: " + myNum1);
   Console.WriteLine("Double: " + myNum2);
-
-//
-
-int age = 49;
-
-Console.WriteLine(age);
