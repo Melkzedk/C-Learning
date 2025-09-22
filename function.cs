66af0043ec0b73bec9b59198e0954f872cfce6af
melkzedek wafula <melkywafula29@gmail.comm>
2025-09-22 21:26:39 +0300

    more changes

--- function.cs
+++ function.cs
@@ -23,3 +23,7 @@ int myNum1 = PlusMethod(8, 5);
   double myNum2 = PlusMethod(4.3, 6.26);
   Console.WriteLine("Int: " + myNum1);
   Console.WriteLine("Double: " + myNum2);
+
+  /*
+ This is the comment
+  */
