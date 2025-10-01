e169c8ec89163a16fd62d26953d07c288e31104e
melkzedek wafula <melkywafula29@gmail.comm>
2025-10-01 13:06:29 +0300

    changes

--- file.cs
+++ file.cs
@@ -1,6 +1,6 @@
 using System.IO;  // include the System.IO namespace
 
-int writeText = "Hello World!";  // Create a text string
+string writeText = "Hello World!";  // Create a text string
 File.WriteAllText("filename.txt", writeText);  // Create a file and write the content of writeText to it
 
 string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
