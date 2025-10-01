84ab3f110aad3b7ff00fd5a5fe1fccb235da62e1
melkzedek wafula <melkywafula29@gmail.comm>
2025-10-01 13:04:56 +0300

    more changes

--- obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443f9392c4cd968e40f4743ab965adef94f78d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4a0f9494f3e8364110ea6e4dbc655c97ea5ced")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
