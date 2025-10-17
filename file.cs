81e779e80266096ead450f67449254857ef7db5f
Melkzedk <melkywafula29@gmail.com>
2025-10-17 10:37:27 +0300

    changes

--- file.cs
+++ file.cs
@@ -8,5 +8,5 @@ Console.WriteLine(readText);  // Output the content
 
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-const string constString = "This is a constant strings.";
+const string constString = "This is a constant string.";
 Console.WriteLine(constString);
